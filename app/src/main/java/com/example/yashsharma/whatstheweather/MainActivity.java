37be3f7213e3bf368b48acd0a3538b7e764f6fb8
Yash <ihm2014006@iiita.ac.in>
2016-04-12 16:41:29 +0530

    display weather info and misc changes

--- app/src/main/java/com/example/yashsharma/whatstheweather/MainActivity.java
+++ app/src/main/java/com/example/yashsharma/whatstheweather/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.yashsharma.whatstheweather;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,19 +20,41 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText cityName;
+    TextView resultTextView;
+
+    public void findWeather(View view){
+        Log.i("city name", cityName.getText().toString());
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(cityName.getWindowToken(), 0);
+
+        try {
+            String encodedCityName = URLEncoder.encode(cityName.getText().toString(), "UTF-8");
+            DownloadTask task = new DownloadTask();
+            task.execute("http://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName
+                    + "&appid=91d4974e57e24b5b51e693b3b6694bb9");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Could not load the weather information.", Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DownloadTask task = new DownloadTask();
-        task.execute("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=91d4974e57e24b5b51e693b3b6694bb9");
+        cityName = (EditText) findViewById(R.id.cityName);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
     }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
@@ -52,10 +80,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return result;
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Could not load the weather information.", Toast.LENGTH_LONG).show();
             }
             return null;
         }
@@ -63,8 +89,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-
             try {
+                String message = "";
                 JSONObject jsonObject = new JSONObject(result);
 
                 String weatherInfo = jsonObject.getString("weather");
@@ -75,12 +101,25 @@ public class MainActivity extends AppCompatActivity {
                 for(int i=0; i < arr.length(); i++){
                     JSONObject jsonPart = arr.getJSONObject(i);
 
-                    Log.i("main", jsonPart.getString("main"));
-                    Log.i("description", jsonPart.getString("description"));
+                    String main = "";
+                    String description = "";
+
+                    main = jsonPart.getString("main");
+                    description = jsonPart.getString("description");
+
+                    if(!main.equals("") && !description.equals("")){
+                        message += main + ": " + description + "\r\n";
+                    }
+                }
+                if(!message.equals("")){
+                    resultTextView.setText(message);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Could not load the weather information.", Toast.LENGTH_LONG).show();
                 }
 
             } catch (JSONException e) {
-                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Could not load the weather information.", Toast.LENGTH_LONG).show();
             }
         }
     }
